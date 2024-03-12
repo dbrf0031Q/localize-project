2c25bf3b73d4b0b28363af41f448024748b53675
Vika <146200003+dbrf0031Q@users.noreply.github.com>
2024-03-12 13:46:06 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+@import '../localise-project/styles/header.scss';
+@import '../localise-project/styles/localise.scss';
+@import '../localise-project/styles/man-revolut-section.scss';
+@import '../localise-project/styles/boost-section.scss';
+@import '../localise-project/styles/section-tort.scss';
+@import '../localise-project/styles/last page.scss';
+@import '../localise-project/styles/section-footer.scss';
+@import '../localise-project/styles/global.scss';
+@import '../localise-project/styles/bird section.scss';
+@import '../localise-project/styles/beige.cards.scss';
+
+@font-face {
+    font-family: 'Graphik LCG';
+    src: url('../localise-project/fonts/transfonter.org-20240220-140411/GraphikLCG-Regular.woff2') format('woff2'),
+        url('../localise-project/fonts/transfonter.org-20240220-140411/GraphikLCG-Regular.woff') format('woff'),
+        url('../localise-project/fonts/transfonter.org-20240220-140411/GraphikLCG-Regular.ttf') format('truetype');
+    font-weight: 600;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Graphik LCG';
+    src: url('../localise-project/fonts/transfonter.org-20240220-140411/GraphikLCG-Semibold.ttf') format('woff2'),
+        url('../localise-project/fonts/transfonter.org-20240220-140411/GraphikLCG-Semibold.woff') format('woff'),
+        url('../localise-project/fonts/transfonter.org-20240220-140411/GraphikLCG-Semibold.woff2') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
